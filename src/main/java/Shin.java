26735956cbb8db411f87514b5cf4e6aec5884326
Arshin Sikka <e1186295@u.nus.edu>
2025-02-05 19:03:39 +0800

    Level-2: Added task storage and listing functionality

--- src/main/java/Shin.java
+++ src/main/java/Shin.java
@@ -8,18 +8,29 @@ public class Shin {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        String input;
+        String[] tasks = new String[100];  // Fixed size array to store tasks
+        int taskCount = 0;
 
         while (true) {
-            input = scanner.nextLine();
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
+            String input = scanner.nextLine();
 
             if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________");
             }
         }
 
