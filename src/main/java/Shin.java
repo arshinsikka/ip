6fef38775e409da0303222d8e219e98d016d51f5
Arshin Sikka <e1186295@u.nus.edu>
2025-02-05 19:09:19 +0800

    Level-3: Added ability to mark tasks as done and undone using Task class

--- src/main/java/Shin.java
+++ src/main/java/Shin.java
@@ -8,25 +8,47 @@ public class Shin {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];  // Fixed size array to store tasks
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
             String input = scanner.nextLine();
+            String[] parts = input.split(" ", 2); // Split command and description
+            String command = parts[0];
 
-            if (input.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if (input.equals("list")) {
+
+            } else if (command.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+
+            } else if (command.equals("mark")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+
+            } else if (command.equals("unmark")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                tasks[taskIndex].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+
             } else {
-                tasks[taskCount] = input;
+                Task newTask = new Task(input);
+                tasks[taskCount] = newTask;
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
